1726917143b2a01911f5321af596c92a3aca766b
overmachado <overbencomo@gmail.com>
2025-06-23 16:58:41 -0400

    Add files via upload

---
+++ Untitled5.py
@@ -0,0 +1,16 @@
+nombre = "Over"
+edad = 17
+ciudad = "maracaibo"
+print (nombre , edad , ciudad)
+
+# nortas de las materias
+matematicas = "a"
+geometria = "b"
+algebra = "c"
+
+a = 18 
+b = 7
+c = 19
+suma = a + b + c
+
+print(suma)
---
+++ pyti.py
@@ -0,0 +1,26 @@
+# sumar dos numeros
+a = 10
+b = 20
+suma = a + b
+print(suma) # outpus: 30
+
+# concatenar cadenas de texto
+saludo = "hola, "
+nombre = "pedro"
+mensaje = saludo + nombre
+print(mensaje) # output: hola, pedro
+
+edad = 1300
+# operacion logicas
+es_adulto = edad > 18
+print(es_adulto)   # output: True
+
+d = 12
+e = 22
+suma = d + e
+resta = d - e
+multiplicacion = d * e
+division = d / e
+division_entera = d // e
+modulo = d % e
+exponiente = d ** e
